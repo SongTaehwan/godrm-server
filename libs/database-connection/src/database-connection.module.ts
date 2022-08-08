f6060177dcf717d91c62d4790e3d862fa1471eae
Song TaeHwan <axoghks@gmail.com>
2022-08-08 17:29:49 +0900

    feat: #3 Implement DatabaseConnection module

---
+++ libs/database-connection/src/database-connection.module.ts
@@ -0,0 +1,8 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URI)],
+  exports: [MongooseModule],
+})
+export class DatabaseConnectionModule {}
