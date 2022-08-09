ffb4715679bd5ca27e577c9d1104eaa4f651ace4
Song TaeHwan <axoghks@gmail.com>
2022-08-09 16:44:24 +0900

    feat: #2 Add Food, Favourite, ShoppingCart database model

---
+++ apps/favourite/src/schema/favourite.schema.ts
@@ -0,0 +1,29 @@
+import { Document, Model, Schema as MongooseSchema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { DEFAULT_SCHEMA_OPTIONS } from '../../../../libs/constants/schema';
+import { User } from '../../../user/src/schema/user.schema';
+import { Food } from 'apps/food/src/schema/food.schema';
+
+export type FavouriteDocument = Favourite & Document;
+export type FavouriteModel = Model<FavouriteDocument>;
+
+@Schema(DEFAULT_SCHEMA_OPTIONS)
+export class Favourite {
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: User.name,
+    required: true,
+  })
+  user: User;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: Food.name,
+    required: true,
+    default: [],
+  })
+  food: Food[];
+}
+
+export const FavouriteSchema = SchemaFactory.createForClass(Favourite);
---
+++ apps/food/src/schema/food.schema.ts
@@ -0,0 +1,68 @@
+import { Document, Model, Schema as MongooseSchema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { DEFAULT_SCHEMA_OPTIONS } from '../../../../libs/constants/schema';
+import { Category } from 'apps/category/src/schema/category.schema';
+import { User } from '../../../user/src/schema/user.schema';
+import { StorageType } from '../dto/create-food.schema';
+
+export type FoodDocument = Food & Document;
+export type FoodModel = Model<FoodDocument>;
+
+@Schema(DEFAULT_SCHEMA_OPTIONS)
+export class Food {
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: Category.name,
+    required: true,
+  })
+  category: Category;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: User.name,
+    required: true,
+  })
+  user: string;
+
+  @Prop({
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    required: true,
+    enum: StorageType,
+    default: StorageType.CHILLED,
+  })
+  storage_type: StorageType;
+
+  @Prop({
+    required: true,
+    default: false,
+  })
+  expired: boolean;
+
+  @Prop({
+    required: true,
+    default: 1,
+  })
+  quantity: number;
+
+  @Prop({
+    default: null,
+  })
+  price: number;
+
+  @Prop({
+    default: null,
+  })
+  expired_at: Date;
+
+  @Prop({
+    default: null,
+  })
+  purchased_at: Date;
+}
+
+export const FoodSchema = SchemaFactory.createForClass(Food);
