1c1de81ca6e082e4029c68caba6b1b6040316ffb
Song TaeHwan <axoghks@gmail.com>
2022-08-09 20:49:40 +0900

    feat: #3 Add scaffolding of ShoppingCart api

--- apps/shopping-cart/src/shopping-cart.controller.ts
+++ apps/shopping-cart/src/shopping-cart.controller.ts
@@ -1,15 +1,56 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import {
+  Get,
+  Body,
+  Patch,
+  Delete,
+  Controller,
+  HttpStatus,
+} from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 
+import { MongoIdValidationPipe } from '../../../libs/common/pipes';
 import { ShoppingCartService } from './shopping-cart.service';
+
+const CART = 'cart';
 
 @Controller()
 export class ShoppingCartController {
   constructor(private readonly shoppingCartService: ShoppingCartService) {}
 
-  @ApiResponse({ status: HttpStatus.OK, description: '장바구니 정보 불러오기' })
-  @Get('cart')
-  getHello(): string {
-    return this.shoppingCartService.getHello();
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '장바구니 데이터 불러오기',
+  })
+  @Get(CART)
+  // TODO: token 을 통해 불러온 유저 정보 삽입 및 장바구니 조회
+  get() {
+    return this.shoppingCartService.getCartByUser('123');
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '장바구니 목록에 아이템 추가',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '추가할 아이템이 존재하지 않음',
+  })
+  @Patch(':id')
+  update(@Body('id', MongoIdValidationPipe) id: string) {
+    return this.shoppingCartService.addItem(id);
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '장바구니 목록 아이템 삭제',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '삭제할 아이템 없음',
+  })
+  @Delete(':id')
+  deleteItem(@Body('id', MongoIdValidationPipe) id: string) {
+    // INFO: 장바구니 테이블 삭제는 User 삭제 시 삭제
+    return this.shoppingCartService.deleteItem(id);
   }
 }
--- apps/shopping-cart/src/shopping-cart.service.ts
+++ apps/shopping-cart/src/shopping-cart.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { ShoppingCart, ShoppingCartModel } from './schema/shopping-cart.schema';
+import { Food } from 'apps/food/src/schema/food.schema';
 
 @Injectable()
 export class ShoppingCartService {
-  getHello(): string {
-    return 'Cart!';
+  constructor(
+    @InjectModel(ShoppingCart.name)
+    private shoppingCartModel: ShoppingCartModel,
+    @InjectModel(Food.name)
+    private foodModel: ShoppingCartModel,
+  ) {}
+
+  async getCartByUser(id: string): Promise<ShoppingCart> {
+    const cart = await this.shoppingCartModel.findOne({ user: id });
+
+    if (!cart) {
+      throw new NotFoundException();
+    }
+
+    return cart;
+  }
+
+  async addItem(id: string) {
+    const item = await this.foodModel.findById(id);
+
+    if (!item) {
+      throw new NotFoundException();
+    }
+
+    // TODO: update cart
+  }
+
+  async deleteItem(id: string) {
+    // const item =
+    // TODO: delete item from cart
   }
 }
