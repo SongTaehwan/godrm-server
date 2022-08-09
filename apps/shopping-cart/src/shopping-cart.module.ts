9e1f0d4d878884462f3e0fc4beec80ae40539171
Song TaeHwan <axoghks@gmail.com>
2022-08-09 20:49:40 +0900

    feat: #3 Add scaffolding of ShoppingCart api

--- apps/shopping-cart/src/shopping-cart.module.ts
+++ apps/shopping-cart/src/shopping-cart.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { FoodModule } from 'apps/food/src/food.module';
 import { ShoppingCartController } from './shopping-cart.controller';
 import { ShoppingCartService } from './shopping-cart.service';
 
 @Module({
-  imports: [],
+  imports: [FoodModule],
   controllers: [ShoppingCartController],
   providers: [ShoppingCartService],
 })
