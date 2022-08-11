0b9746200700b0fb036696d5c8c0a544a4f440ae
Song TaeHwan <axoghks@gmail.com>
2022-08-11 19:03:19 +0900

    refactor: Move custom decorator and guards under libs

--- libs/common/guards/jwt-auth.guard.ts
+++ libs/common/guards/jwt-auth.guard.ts
@@ -23,6 +23,9 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
 
+    // canActivate 호출 중 관련 Custom Strategy 도 호출되는데 라이브러리의 Strategy 에서 request body 정보 validation 중 invalid 판단 시 canActivate 에서 error throw
+    // 유효하면 Strategy 의 validate 호출 후 request.user 에 삽입
+    // canActivate 종료
     return super.canActivate(context);
   }
 }
