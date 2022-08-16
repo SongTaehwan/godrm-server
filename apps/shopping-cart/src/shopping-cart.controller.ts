17f7ee3e0138039aad43f62d0dc5954cda2c63f7
Song TaeHwan <axoghks@gmail.com>
2022-08-16 19:00:40 +0900

    feat: #3 Implement API for shopping cart

--- apps/shopping-cart/src/shopping-cart.controller.ts
+++ apps/shopping-cart/src/shopping-cart.controller.ts
@@ -2,7 +2,6 @@ import {
   Get,
   Body,
   Post,
-  Patch,
   Param,
   Delete,
   Controller,
@@ -10,15 +9,12 @@ import {
 } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+import { SHOPPING_CART, ID, ID_PARAMETER, CART } from './constants';
 import { MongoIdValidationPipe } from '../../../libs/common/pipes';
 import { Private, User } from '../../../libs/common/decorators';
 import { ShoppingCartService } from './shopping-cart.service';
 
-const CART = 'cart';
-const ID = 'id';
-const ID_PARAMETER = ':id';
-
-@ApiTags('Shopping Cart')
+@ApiTags(SHOPPING_CART)
 @Private()
 @Controller()
 export class ShoppingCartController {
@@ -30,18 +26,35 @@ export class ShoppingCartController {
   })
   @Get(CART)
   getByUser(@User(ID) id: string) {
-    return this.shoppingCartService.getCartByUser(id);
+    return this.shoppingCartService.getCart({ user: id });
   }
 
   @ApiResponse({
     status: HttpStatus.OK,
     description: '특정 장바구니 목록 불러오기',
   })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '장바구니 목록 존재하지 않음',
+  })
   @Get(ID_PARAMETER)
   getById(@Param(ID, MongoIdValidationPipe) id: string) {
-    return this.shoppingCartService.getCartById(id);
+    return this.shoppingCartService.getCart({ id });
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '특정 장바구니 목록 불러오기',
+  })
+  @Get(`${ID_PARAMETER}/items`)
+  getCartItems(@Param(ID, MongoIdValidationPipe) id: string) {
+    return this.shoppingCartService.getItems(id);
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '장바구니 목록 생성',
+  })
   @Post(CART)
   create(@User(ID) id: string) {
     return this.shoppingCartService.create(id);
@@ -55,9 +68,12 @@ export class ShoppingCartController {
     status: HttpStatus.NOT_FOUND,
     description: '추가할 아이템이 존재하지 않음',
   })
-  @Patch(ID_PARAMETER)
-  update(@Body(ID, MongoIdValidationPipe) id: string) {
-    return this.shoppingCartService.addItem(id);
+  @Post(`${ID_PARAMETER}/items`)
+  update(
+    @Param(ID, MongoIdValidationPipe) id: string,
+    @Body(ID, MongoIdValidationPipe) itemId: string,
+  ) {
+    return this.shoppingCartService.addItem({ id, itemId });
   }
 
   @ApiResponse({
@@ -68,27 +84,30 @@ export class ShoppingCartController {
     status: HttpStatus.NOT_FOUND,
     description: '삭제할 아이템 없음',
   })
-  @Delete(`${CART}/item/${ID_PARAMETER}`)
-  deleteItem(@Param(ID, MongoIdValidationPipe) id: string) {
-    return this.shoppingCartService.deleteItem(id);
+  @Delete(`${ID_PARAMETER}/items`)
+  deleteItem(
+    @Param(ID, MongoIdValidationPipe) id: string,
+    @Body(ID, MongoIdValidationPipe) itemId: string,
+  ) {
+    return this.shoppingCartService.removeItem({ id, itemId });
   }
 
   @ApiResponse({
     status: HttpStatus.OK,
-    description: '장바구니 목록 삭제',
+    description: '특정 장바구니 목록 데이터 삭제',
   })
   @ApiResponse({
     status: HttpStatus.NOT_FOUND,
     description: '삭제할 장바구니 목록 없음',
   })
   @Delete(ID_PARAMETER)
-  delete(@Param(ID) id: string) {
-    return this.shoppingCartService.delete(id);
+  delete(@Param(ID, MongoIdValidationPipe) id: string) {
+    return this.shoppingCartService.delete({ id });
   }
 
   @ApiResponse({
     status: HttpStatus.OK,
-    description: '장바구니 목록 삭제',
+    description: 'API 요청한 사용자의 장바구니 목록 데이터 삭제',
   })
   @ApiResponse({
     status: HttpStatus.NOT_FOUND,
@@ -96,6 +115,6 @@ export class ShoppingCartController {
   })
   @Delete(CART)
   deleteByUser(@User(ID) id: string) {
-    return this.shoppingCartService.delete(id);
+    return this.shoppingCartService.delete({ user: id });
   }
 }
