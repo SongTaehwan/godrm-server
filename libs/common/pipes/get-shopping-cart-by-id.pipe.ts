99a11a018a407bb87a9a9c30af0cee5c4180c7c4
Song TaeHwan <axoghks@gmail.com>
2022-08-16 18:34:46 +0900

    feat: Implement utiliy pipes

---
+++ libs/common/pipes/get-shopping-cart-by-id.pipe.ts
@@ -0,0 +1,44 @@
+import {
+  Injectable,
+  PipeTransform,
+  ArgumentMetadata,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isMongoId } from 'class-validator';
+
+import {
+  ShoppingCart,
+  ShoppingCartModel,
+  ShoppingCartDocument,
+} from '../../../apps/shopping-cart/src/schema/shopping-cart.schema';
+
+@Injectable()
+export class GetShoppingCartById
+  implements PipeTransform<string, Promise<ShoppingCartDocument>>
+{
+  constructor(
+    @InjectModel(ShoppingCart.name)
+    private shoppingCartModel: ShoppingCartModel,
+  ) {}
+
+  async transform(
+    value: string,
+    metadata: ArgumentMetadata,
+  ): Promise<ShoppingCartDocument> {
+    if (!isMongoId(value)) {
+      throw new BadRequestException(`Invalid ID: ${value}`);
+    }
+
+    const cart = await this.shoppingCartModel.findOne({
+      $or: [{ id: value }, { user: value }],
+    });
+
+    if (!cart) {
+      throw new NotFoundException();
+    }
+
+    return cart;
+  }
+}
